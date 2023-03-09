151b2ecea0e2d47bda64b9ce88f9b854f74fc02d
repeater1384 <repeater1384@naver.com>
2023-03-09 13:48:27 +0900

    [ADD] : init project2

--- ilkyu/pythonProject1/main.py
+++
@@ -1,7 +0,0 @@
-sum = 0
-a = 357
-for i in range(6):
-    sum += a + a * i
-    print(i + 1, ':', a + a * i)
-
-print('sum : ', sum)
--- ilkyu/pythonProject1/youtube_script.py
+++
@@ -1,5 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-for row in YouTubeTranscriptApi.get_transcript('WUvTyaaNkzM', languages=['ko']):
-    if len(row['text'].strip().split()) >= 1:
-        print(row)
