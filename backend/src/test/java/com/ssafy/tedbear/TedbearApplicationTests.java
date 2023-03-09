1b341c5afea250aa515d6adfaf0ab3a32433505d
repeater1384 <repeater1384@naver.com>
2023-03-09 15:42:34 +0900

    [ADD] : 폴더구조 변경

---
+++ backend/src/test/java/com/ssafy/tedbear/TedbearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tedbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedbearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
