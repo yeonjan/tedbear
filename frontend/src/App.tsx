5d603291594be33571b309a0d21b141b81936796
정혜주 <fixup719@gmail.com>
2023-03-13 14:00:15 +0900

    :lipstick: 라이트 모드 색상 theme.ts 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from 'pages/HomePage'
-import LandingPage from 'pages/LandingPage'
-import GamePage from 'pages/GamePage'
-import CollectionPage from 'pages/CollectionPage'
-import ProfilePage from 'pages/ProfilePage'
-import LayoutPage from 'pages/LayoutPage'
-import TestPage from 'pages/TestPage'
-import GlobalStyle from 'GlobalStyle';
-
-
+import HomePage from "pages/HomePage";
+import LandingPage from "pages/LandingPage";
+import GamePage from "pages/GamePage";
+import CollectionPage from "pages/CollectionPage";
+import ProfilePage from "pages/ProfilePage";
+import LayoutPage from "pages/LayoutPage";
+import TestPage from "pages/TestPage";
+import GlobalStyle from "GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "theme";
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<LayoutPage/>}>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/game" element={<GamePage />} />
-            <Route path="/collection" element={<CollectionPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/test" element={<TestPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<LayoutPage />}>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/game" element={<GamePage />} />
+              <Route path="/collection" element={<CollectionPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/test" element={<TestPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ frontend/src/theme.ts
@@ -0,0 +1,35 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  mainColor: "#6255A4",
+  mainLightColor: "#8F84CE",
+  pointColor: "#E67F51",
+  pointLightColor: "#FEAD55",
+  // pointLigntColor 그라데이션
+  pointLigntGrdColor1: "#FEB668",
+  pointLigntGrdColor2: "#FEBF7B",
+  pointLigntGrdColor3: "#FEC88E",
+  pointLigntGrdColor4: "#FED1A1",
+  pointLigntGrdColor5: "#FFDBB3",
+  pointLigntGrdColor6: "#FFE4C6",
+  pointLigntGrdColor7: "#FFEDD9",
+  pointLigntGrdColor8: "#FFF6EC",
+  bgColor: "#EAEAEA",
+  whiteColor: "#FFFFFF",
+  blackColor: "#000000",
+  redColor: "#FF0000",
+  shadowColor: "#BFBFBF",
+  kakaoColor: "#FEE500",
+  naverColor: "#03C75A",
+  landingWavseGrdColor: "#7968D0",
+  landingWaveColor: "#968EC2",
+  landingWaveLigntColor: "#CBC6E1",
+  landingCircleColor: "#E3E1EA",
+  learningBoxDefaultColor: "#F3F1FA",
+  learningBoxIncorrectColor: "#FFEBEB",
+  learningBoxCorrect: "#EBFFEC",
+  blackColorLight1: "#7B7B7B",
+  blackColorLight2: "#8E8E8E",
+};
+
+export const darkTheme: DefaultTheme = {};
