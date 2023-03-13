4b9416265a6d7fecbc6ce4b3d058058c5452456c
정혜주 <fixup719@gmail.com>
2023-03-13 15:01:12 +0900

    :lipstick: styled.d.ts defaultTheme type 설정

---
+++ frontend/src/styled.d.ts
@@ -0,0 +1,35 @@
+import { DefaultTheme } from 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    mainColor: string;
+    mainLightColor: string;
+    pointColor: string;
+    pointLightColor: string;
+    // pointLigntColor 그라데이션
+    pointLigntGrdColor1: string;
+    pointLigntGrdColor2: string;
+    pointLigntGrdColor3: string;
+    pointLigntGrdColor4: string;
+    pointLigntGrdColor5: string;
+    pointLigntGrdColor6: string;
+    pointLigntGrdColor7: string;
+    pointLigntGrdColor8: string;
+    bgColor: string;
+    whiteColor: string;
+    blackColor: string;
+    redColor: string;
+    shadowColor: string;
+    kakaoColor: string;
+    naverColor: string;
+    landingWavseGrdColor: string;
+    landingWaveColor: string;
+    landingWaveLigntColor: string;
+    landingCircleColor: string;
+    learningBoxDefaultColor: string;
+    learningBoxIncorrectColor: string;
+    learningBoxCorrect: string;
+    blackColorLight1: string;
+    blackColorLight2: string;
+  }
+}
