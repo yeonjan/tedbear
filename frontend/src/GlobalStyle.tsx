091f9a09c490de9382f13f575110c306c107ec2c
seunghwan <asdryzx@naver.com>
2023-03-13 14:41:40 +0900

    :art: 저장

--- frontend/src/GlobalStyle.tsx
+++ frontend/src/GlobalStyle.tsx
@@ -1,12 +1,12 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
     box-sizing: border-box;
     margin: 0;
-  padding: 0;
-  border: 0;
+    padding: 0;
+    border: 0;
   }
-`
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
