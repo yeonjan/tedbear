707a86beae852d0be1f1b4f7b106f89460ac3372
정혜주 <fixup719@gmail.com>
2023-03-13 14:38:48 +0900

    :lipstick: styled-component color test

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,21 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  /* width: 100vh; */
+  /* height: 100vh; */
+  background-color: ${props => props.theme.mainColor};
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.pointLigntGrdColor1};
+`;
+
 const LandingPage = () => {
   return (
-    <div>
-      <h1>LandingPage</h1>
-    </div>
+    <Box>
+      <Title>LandingPage</Title>
+    </Box>
   );
 };
 
