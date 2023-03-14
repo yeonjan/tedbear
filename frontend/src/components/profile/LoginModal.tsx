6a15ce6276e211da38b87a779aeb1fb6353ed3e2
seunghwan <asdryzx@naver.com>
2023-03-14 14:01:20 +0900

    :art: img 적용

--- frontend/src/components/profile/LoginModal.tsx
+++ frontend/src/components/profile/LoginModal.tsx
@@ -1,24 +1,39 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+import ModalBg from 'assets/img/ModalMan.png';
 
 const DarkBackground = styled.div`
   background-color: rgba(0, 0, 0, 0.2);
   width: 100vw;
   height: 100vh;
-  z-index: 0;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   position: absolute;
 `;
 
-const LoginModal = (
-  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>,
-) => {
+const Modal = styled.div`
+  border: 1px solid black;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  height: 50vh;
+  background-image: url(${ModalBg});
+  z-index: 1;
+  border-radius: 16px;
+  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.04);
+`;
+
+interface Props {
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoginModal = ({ setOpenModal }: Props) => {
   return (
     <div>
-      <DarkBackground onClick={() => setOpenModal(false)}></DarkBackground>
-      <div>Hi</div>)
+      <DarkBackground onClick={() => setOpenModal(false)} />
+      <Modal></Modal>
     </div>
   );
 };
