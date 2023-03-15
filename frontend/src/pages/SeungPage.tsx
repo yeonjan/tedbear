5148ef8d329f24ffcedbd49f9550197757f58702
seunghwan <asdryzx@naver.com>
2023-03-15 17:28:20 +0900

    :art: login

--- frontend/src/pages/SeungPage.tsx
+++ frontend/src/pages/SeungPage.tsx
@@ -9,6 +9,23 @@ const SeungPage = () => {
       <h1>SeungPage</h1>
       <button onClick={() => setModalOpen(true)}>Start!</button>
       {modalOpen && <LoginModal setOpenModal={setModalOpen} />}
+      <div style={{ display: 'flex', marginTop: '10px ' }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <button>안녕</button>
+          <button>안녕</button>
+          <button>안녕</button>
+        </div>
+        <div
+          style={{
+            background: 'black',
+            width: '40vw',
+            height: '40vh',
+            color: 'white',
+          }}
+        >
+          단어장
+        </div>
+      </div>
     </div>
   );
 };
