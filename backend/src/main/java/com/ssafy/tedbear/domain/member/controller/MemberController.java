5339b738856430b95f6b6edd5c497e73acef136c
박서영 <gksdidtk123@gmail.com>
2023-03-15 17:59:47 +0900

    :art: MemberController 생성

---
+++ backend/src/main/java/com/ssafy/tedbear/domain/member/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.ssafy.tedbear.domain.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/member")
+@NoArgsConstructor
+@Slf4j
+public class MemberController {
+	
+}
