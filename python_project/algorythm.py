ff5893081160dd45c455f1b9b311e840d8009e49
repeater1384 <repeater1384@naver.com>
2023-03-16 15:04:04 +0900

    [MOD] : 번역 테스트

--- python_project/algorythm.py
+++ python_project/algorythm.py
@@ -1,2 +1,21 @@
-print(int(input())**2)
-print(2)
+from collections import deque
+
+N = int(input())
+arr = [(idx + 1, move) for idx, move in enumerate(map(int, input().split()))]
+queue = deque(arr)
+answer = []
+
+while queue:
+    idx, move = queue.popleft()
+    answer.append(idx)
+    if move > 0:
+        move -= 1
+    while move and queue:
+        if move > 0:
+            queue.append(queue.popleft())
+            move -= 1
+        else:
+            queue.appendleft(queue.pop())
+            move += 1
+
+print(*answer)
--- python_project/ec2test.py
+++ python_project/ec2test.py
@@ -1,1 +1,28 @@
-print('your test success')
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+chrome_driver = ChromeDriverManager().install()
+driver = webdriver.Chrome(chrome_driver)
+
+URL = 'https://papago.naver.com/?sk=en&tk=ko'
+driver.get(URL)
+time.sleep(3)
+
+question = input('번역할 단어를 입력하세요 : ')
+
+form = driver.find_element(By.CSS_SELECTOR, "textarea#txtSource")
+form.send_keys(question)
+
+button = driver.find_element(By.CSS_SELECTOR, "button#btnTranslate")
+button.click()
+
+while True:
+    time.sleep(2)
+    result = driver.find_element(By.CSS_SELECTOR, "div#txtTarget")
+    if result:
+        break
+print(question, "->", result.text)
+
+driver.close()
