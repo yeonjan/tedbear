b0a911234e232213e9497562df7a0ea93f7ae273
yeonjin <y_jin99@naver.com>
2023-03-17 16:28:15 +0900

    :rocket: add Dockerfile

--- backend/src/main/java/com/ssafy/tedbear/TedbearApplication.java
+++ backend/src/main/java/com/ssafy/tedbear/TedbearApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TedbearApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TedbearApplication.class, args);
 	}
