95ac578dc1546320891819d5fb9970585e551f88
hwanny7 <asdryzx2@gmail.com>
2023-03-19 02:06:51 +0900

    :sparkles: 유튜브 쇼츠 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -11,36 +11,36 @@ interface Props {
 const HomePage = () => {
   const data: Props[] = [
     {
-      url: 'https://img.youtube.com/vi/6Af6b_wyiwI/0.jpg',
+      url: 'https://img.youtube.com/vi/6Af6b_wyiwI/maxresdefault.jpg',
       id: '6Af6b_wyiwI',
     },
     {
-      url: 'https://img.youtube.com/vi/RLESBHduKBs/0.jpg',
+      url: 'https://img.youtube.com/vi/RLESBHduKBs/maxresdefault.jpg',
       id: 'RLESBHduKBs',
     },
     {
-      url: 'https://img.youtube.com/vi/wL8X31XWZW8/0.jpg',
+      url: 'https://img.youtube.com/vi/wL8X31XWZW8/maxresdefault.jpg',
       id: 'wL8X31XWZW8',
     },
     {
-      url: 'https://img.youtube.com/vi/BEBKC7Hqfr0/0.jpg',
+      url: 'https://img.youtube.com/vi/BEBKC7Hqfr0/maxresdefault.jpg',
       id: 'BEBKC7Hqfr0',
     },
     {
-      url: 'https://img.youtube.com/vi/LDVyOnf0t9M/0.jpg',
+      url: 'https://img.youtube.com/vi/LDVyOnf0t9M/maxresdefault.jpg',
       id: 'LDVyOnf0t9M',
     },
     {
-      url: 'https://img.youtube.com/vi/JH_Pa1hOEVc/0.jpg',
+      url: 'https://img.youtube.com/vi/JH_Pa1hOEVc/maxresdefault.jpg',
       id: 'JH_Pa1hOEVc',
     },
     {
-      url: 'https://img.youtube.com/vi/9XGm_uHit5g/0.jpg',
+      url: 'https://img.youtube.com/vi/9XGm_uHit5g/maxresdefault.jpg',
       id: '9XGm_uHit5g',
     },
   ];
   return (
-    <Main style={{ width: '80%', marginLeft: '10%' }}>
+    <Main style={{ marginLeft: '10%' }}>
       <Carousel data={data}></Carousel>
     </Main>
   );
