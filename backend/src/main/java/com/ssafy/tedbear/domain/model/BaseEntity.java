91f6ea06c9cf4434d670f832f5a024b204290c49
yeonjin <y_jin99@naver.com>
2023-03-20 16:47:32 +0900

    :art: @Builder to @SuperBuilder

--- backend/src/main/java/com/ssafy/tedbear/domain/model/BaseEntity.java
+++ backend/src/main/java/com/ssafy/tedbear/domain/model/BaseEntity.java
@@ -9,10 +9,14 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Getter
+@SuperBuilder
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
 public abstract class BaseEntity {
 
 	@CreatedDate
