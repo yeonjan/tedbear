7f1fc372aefd5a17c9bff86142aa01215e5af928
정혜주 <fixup719@gmail.com>
2023-03-20 17:54:06 +0900

    :lipstick: 랜딩 반응형 적용 마크업

--- frontend/src/pages/LandingPageTest.tsx
+++ frontend/src/pages/LandingPageTest.tsx
@@ -1,8 +1,13 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { device } from 'utils/mediaQuery';
 // visual svg
-import Wave from 'assets/img/wave.svg';
+import LandingVisual from 'assets/img/landingVisual.svg';
 import Smog from 'assets/img/landingSmog.svg';
+import Cloud from 'assets/img/landingCloud.svg';
+import LandingBear from 'assets/img/landingBear.svg';
+// toggle svg
+import Sun from 'assets/img/sun.svg';
+import Moon from 'assets/img/moon.svg';
 import { SetStateAction } from 'react';
 
 // 인터페이스
@@ -17,15 +22,16 @@ interface ToggleStyledProps {
 
 // /////////////////////////////////////////////////////////////
 const Wrapper = styled.div`
-  border: 1px solid black;
+  background-color: ${props => props.theme.bgColor};
 `;
 
 // 상단 비주얼 /////////////////////////////////////////////////////////////////////////////////////////
 const Visual = styled.div`
-  background-color: ${props => props.theme.mainColor};
-  width: 100%;
+  width: 100vw;
   display: flex;
   flex-direction: column;
+  position: relative;
+  background-color: ${props => props.theme.mainColor};
 
   @media ${device.mobile} {
     height: 500px;
@@ -40,8 +46,19 @@ const Visual = styled.div`
   }
 
   @media ${device.desktop} {
-    height: 800px;
+    height: 700px;
   }
+`;
+
+// 사이트 대문 배경
+const LandingVisualImg = styled.img<ToggleStyledProps>`
+  position: absolute;
+  width: 100%;
+  bottom: 0%;
+  filter: ${ToggleStyledProps =>
+    !ToggleStyledProps.toggle
+      ? 'invert(100%) sepia(6%) saturate(32%) hue-rotate(244deg) brightness(113%) contrast(84%)'
+      : 'invert(13%) sepia(0%) saturate(1229%) hue-rotate(166deg) brightness(94%) contrast(76%)'};
 `;
 
 // 라이트모드, 다크모드 설정 토글
@@ -49,18 +66,56 @@ const ToggleBox = styled.div`
   /* background-color: ${props => props.theme.mainColor}; */
   width: 100%;
   height: 10%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  padding-right: 32px;
+`;
 
-  @media ${device.mobile} {
-  }
+const ToggleBtn = styled.div`
+  background-image: -webkit-linear-gradient(
+    #b3b3b3 0%,
+    #ececec 80%,
+    #ececec 100%
+  );
+  border-radius: 50px;
+  position: relative;
+  width: 56px;
+  height: 16px;
+  display: flex;
+  align-items: center;
+`;
 
-  @media ${device.tablet} {
-  }
+const Circle = styled.div<ToggleStyledProps>`
+  background: ${ToggleStyledProps =>
+    !ToggleStyledProps.toggle
+      ? 'linear-gradient(270deg, #4cb0b9 0%, #47bcc7 30%, #65e3ee 80%)'
+      : 'linear-gradient(90deg, #000000 0%, #3f0e63 90%, #331056 100%)'};
+  width: 32px;
+  height: 32px;
+  z-index: 999;
+  position: absolute;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 2px 2px 4px #00000053;
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s;
+  ${ToggleStyledProps =>
+    ToggleStyledProps.toggle &&
+    `
+      transform: translateX(24px);
+    `}
+`;
 
-  @media ${device.laptop} {
-  }
+const SunImg = styled.img`
+  width: 70%;
+`;
 
-  @media ${device.desktop} {
-  }
+const MoonImg = styled(SunImg)`
+  width: 100%;
 `;
 
 // 사이트 대문
@@ -70,38 +125,28 @@ const VisualContent = styled.div`
   display: flex;
   position: relative;
 
+  align-items: center;
+  text-align: center;
+
   @media ${device.mobile} {
     flex-direction: column;
+    justify-content: center;
   }
 
   @media ${device.tablet} {
     flex-direction: column;
+    justify-content: center;
   }
 
   @media ${device.laptop} {
     flex-direction: row;
+    justify-content: start;
   }
 
   @media ${device.desktop} {
     flex-direction: row;
+    justify-content: start;
   }
-`;
-
-// 사이트 대문 배경 물결
-const WaveImg1 = styled.img<ToggleStyledProps>`
-  width: 100%;
-  left: 0;
-  z-index: 0;
-  position: absolute;
-  bottom: 0;
-`;
-
-const WaveImg2 = styled(WaveImg1)`
-  bottom: -10px;
-`;
-
-const WaveImg3 = styled(WaveImg1)`
-  bottom: -20px;
 `;
 
 // 사이트 대문 컨텐트 - Text / Img / Button
@@ -109,15 +154,16 @@ const TextBox = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
+  border: 1px solid red;
 
   @media ${device.mobile} {
     width: 100%;
-    height: 50%;
+    height: 40%;
   }
 
   @media ${device.tablet} {
     width: 100%;
-    height: 50%;
+    height: 40%;
   }
 
   @media ${device.laptop} {
@@ -126,7 +172,7 @@ const TextBox = styled.div`
   }
 
   @media ${device.desktop} {
-    width: 65%;
+    width: 60%;
     height: 100%;
   }
 `;
@@ -199,26 +245,186 @@ const SubTitle = styled.div`
 `;
 
 const ImgBox = styled.div`
-  /* border: 1px solid blue; */
+  border: 1px solid red;
+  position: relative;
+  display: flex;
+  justify-content: center;
 
   @media ${device.mobile} {
-    width: 100%;
-    height: 50%;
+    padding-top: 10px;
+    width: 50%;
+    height: 60%;
   }
 
   @media ${device.tablet} {
-    width: 100%;
-    height: 50%;
+    padding-top: 10px;
+    width: 50%;
+    height: 60%;
   }
 
   @media ${device.laptop} {
-    width: 40%;
+    width: 350px;
     height: 100%;
   }
 
   @media ${device.desktop} {
-    width: 35%;
+    width: 450px;
     height: 100%;
+  }
+`;
+
+const SmogImg = styled.img`
+  position: absolute;
+
+  @media ${device.mobile} {
+    /* top: 0px; */
+    /* left: 0px; */
+    width: 200px;
+  }
+
+  @media ${device.tablet} {
+    width: 200px;
+  }
+
+  @media ${device.laptop} {
+    top: 100px;
+    width: 300px;
+  }
+
+  @media ${device.desktop} {
+    top: 20px;
+    left: 10px;
+    width: 400px;
+  }
+
+  @media (min-width: 1166px) {
+    top: 10px;
+    left: 20px;
+    width: 400px;
+  }
+`;
+
+const CloudImg = styled.img`
+  position: absolute;
+
+  @media ${device.mobile} {
+    width: 80px;
+    top: 40px;
+    left: 0px;
+  }
+
+  @media ${device.tablet} {
+    width: 80px;
+    top: 40px;
+    left: 10px;
+  }
+
+  @media (min-width: 600px) {
+    width: 80px;
+    top: 40px;
+    left: 40px;
+  }
+
+  @media (min-width: 700px) {
+    width: 80px;
+    top: 40px;
+    left: 70px;
+  }
+
+  @media ${device.laptop} {
+    top: 150px;
+    width: 120px;
+    left: 0px;
+  }
+
+  @media ${device.desktop} {
+    top: 100px;
+    width: 160px;
+    left: 0px;
+  }
+
+  @media (min-width: 1166px) {
+    top: 90px;
+    width: 160px;
+    left: 0px;
+  }
+`;
+
+const CloudImg2 = styled.img`
+  position: absolute;
+
+  @media ${device.mobile} {
+    width: 60px;
+    top: 70px;
+    right: 0px;
+  }
+
+  @media ${device.tablet} {
+    width: 60px;
+    top: 70px;
+    right: 30px;
+  }
+
+  @media (min-width: 600px) {
+    width: 60px;
+    top: 70px;
+    right: 50px;
+  }
+
+  @media (min-width: 700px) {
+    width: 60px;
+    top: 70px;
+    right: 90px;
+  }
+
+  @media ${device.laptop} {
+    width: 100px;
+    top: 190px;
+    right: 50px;
+  }
+
+  @media ${device.desktop} {
+    width: 120px;
+    top: 160px;
+    right: 50px;
+  }
+
+  @media (min-width: 1166px) {
+    width: 120px;
+    top: 150px;
+    right: 100px;
+  }
+`;
+
+const LandingBearImg = styled.img`
+  position: absolute;
+
+  @media ${device.mobile} {
+    top: 40px;
+    /* left: 0px; */
+    width: 180px;
+  }
+
+  @media ${device.tablet} {
+    top: 40px;
+    width: 200px;
+  }
+
+  @media ${device.laptop} {
+    top: 150px;
+    width: 300px;
+  }
+
+  @media ${device.desktop} {
+    top: 100px;
+    left: 10px;
+    width: 400px;
+  }
+
+  @media (min-width: 1166px) {
+    top: 90px;
+    left: 20px;
+    width: 400px;
   }
 `;
 
@@ -259,8 +465,9 @@ const StartBtn = styled.button`
 
 // 내용
 const Content = styled.div`
-  background-color: green;
+  /* border: 1px solid black; */
   min-height: 600px;
+  background-color: red;
 `;
 
 const LandingPageTest = (props: Props) => {
@@ -272,11 +479,15 @@ const LandingPageTest = (props: Props) => {
   return (
     <Wrapper>
       <Visual>
-        <ToggleBox></ToggleBox>
+        <LandingVisualImg src={LandingVisual} toggle={props.toggle} />
+        <ToggleBox>
+          <ToggleBtn>
+            <Circle onClick={clickedToggle} toggle={props.toggle}>
+              {props.toggle ? <MoonImg src={Moon} /> : <SunImg src={Sun} />}
+            </Circle>
+          </ToggleBtn>
+        </ToggleBox>
         <VisualContent>
-          <WaveImg1 src={Wave} toggle={props.toggle} />
-          <WaveImg2 src={Wave} toggle={props.toggle} />
-          <WaveImg3 src={Wave} toggle={props.toggle} />
           <TextBox>
             <Title>
               Let&apos;s Learn with <span>TedBear</span>
@@ -284,7 +495,12 @@ const LandingPageTest = (props: Props) => {
             <SubTitle>TedBear로 영어 스피킹 연습을 해보세요!</SubTitle>
             <StartBtn>Get Started</StartBtn>
           </TextBox>
-          <ImgBox></ImgBox>
+          <ImgBox>
+            <SmogImg src={Smog} />
+            <CloudImg src={Cloud} />
+            <CloudImg2 src={Cloud} />
+            <LandingBearImg src={LandingBear} />
+          </ImgBox>
         </VisualContent>
       </Visual>
       <Content></Content>
