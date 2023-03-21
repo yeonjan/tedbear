56bf112c80f695cbfae0256efad67d525f0356e4
jyuha <jyuha759@gmail.com>
2023-03-21 11:00:26 +0900

    💄 Making a Cirsular Streak

--- frontend/src/pages/YuhaPage.tsx
+++ frontend/src/pages/YuhaPage.tsx
@@ -3,11 +3,13 @@ import Exp from 'components/profile/Exp';
 import HeatMap from 'components/profile/Heatmap';
 import Level from 'components/profile/Level';
 import Streak from 'components/profile/Streak';
+import CircularStreak from 'components/profile/CircularStreak';
 
 const YuhaPage = () => {
   return (
     <div>
       <h1>YuhaPage</h1>
+      <CircularStreak></CircularStreak>
       <HeatMap></HeatMap>
       <Streak></Streak>
       <Level></Level>
