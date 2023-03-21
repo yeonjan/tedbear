9c68175779e6dadf4af6d28aec59a227b445a441
repeater1384 <repeater1384@naver.com>
2023-03-21 12:33:15 +0900

    :white_check_mark: Junit 테스트 코드 추가

---
+++ backend/src/test/java/com/ssafy/tedbear/domain/video/service/VideoServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.ssafy.tedbear.domain.video.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VideoServiceImplTest {
+
+	@Test
+	public void getId(){
+
+	}
+}
