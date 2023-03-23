49e53432d349ba39fce56b4f49184c56f63e618c
jyuha <jyuha759@gmail.com>
2023-03-23 16:21:39 +0900

    💄 BookMark UI

--- frontend/src/components/profile/BookmarkWord.tsx
+++ frontend/src/components/profile/BookmarkWord.tsx
@@ -1,8 +1,17 @@
+import styled from 'styled-components';
+const BookIn = styled.div`
+  position: absolute;
+  max-height: 800px;
+  overflow: auto;
+  margin: 20px;
+`;
 const BookmarkWord = () => {
   return (
-    <div>
-      <h2>BookmarkWord</h2>
-    </div>
+    <BookIn>
+      <div>
+        <h2>BookmarkWord</h2>
+      </div>
+    </BookIn>
   );
 };
 
