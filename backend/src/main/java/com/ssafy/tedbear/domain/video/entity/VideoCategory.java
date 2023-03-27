883a50917d60a7e06dfc5b4e4e7074e262bedbe6
repeater1384 <repeater1384@naver.com>
2023-03-27 15:36:59 +0900

    :necktie: 유저 Level API 개발 완료

--- backend/src/main/java/com/ssafy/tedbear/domain/video/entity/VideoCategory.java
+++ backend/src/main/java/com/ssafy/tedbear/domain/video/entity/VideoCategory.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "video_category_tb")
 @AllArgsConstructor
-@NoArgsConstructor(access = Ac qcessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class VideoCategory {
 	@Id
