0461b7cdeb7f6c23e930c2e9b784dcf3817c3f41
jyuha <jyuha759@gmail.com>
2023-03-27 10:54:51 +0900

    💄 Mission Complete UI

--- frontend/src/pages/GameCompletePage.tsx
+++ frontend/src/pages/GameCompletePage.tsx
@@ -1,8 +1,43 @@
+import { Button } from '@mui/material';
+import { Stack } from '@mui/system';
+import { ReactComponent as Complete } from 'assets/img/complete.svg';
+import styled from 'styled-components';
+
+const StyledComplete = styled.div`
+  .whole {
+    position: relative;
+  }
+  .complete-bg {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+  }
+  .buttons {
+  }
+  .button-1 {
+  }
+  .button-2 {
+  }
+`;
+
 const GameCompletePage = () => {
   return (
-    <div>
-      <h1>GameCompletePage</h1>
-    </div>
+    <StyledComplete>
+      <div className="whole">
+        <Complete className="complete-bg">
+          <Stack
+            className="
+        buttons"
+          >
+            <Button className="button-1"></Button>
+            <Button className="button-2"></Button>
+          </Stack>
+        </Complete>
+      </div>
+    </StyledComplete>
   );
 };
 
