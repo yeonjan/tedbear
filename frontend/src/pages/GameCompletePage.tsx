b7e856d649d389952c24361f116d30f1c94d68e7
jyuha <jyuha759@gmail.com>
2023-03-27 13:08:42 +0900

    💄 Puzzle Game Complete Page UI

--- frontend/src/pages/GameCompletePage.tsx
+++ frontend/src/pages/GameCompletePage.tsx
@@ -1,41 +1,70 @@
 import { Button } from '@mui/material';
 import { Stack } from '@mui/system';
-import { ReactComponent as Complete } from 'assets/img/complete.svg';
+import Complete from 'assets/img/complete.svg';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledComplete = styled.div`
-  .whole {
-    position: relative;
-  }
-  .complete-bg {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
-  }
-  .buttons {
-  }
-  .button-1 {
-  }
-  .button-2 {
-  }
+  height: 100%;
+  width: 100%;
+  background-image: url(${Complete});
+  background-size: contain;
+  background-repeat: no-repeat;
+  justify-content: center;
 `;
 
 const GameCompletePage = () => {
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate('/home');
+  };
+  const handleRe = () => {
+    navigate('/game');
+  };
   return (
     <StyledComplete>
       <div className="whole">
-        <Complete className="complete-bg">
-          <Stack
-            className="
-        buttons"
+        <Stack
+          className="buttons"
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+        >
+          <Button
+            className="button-1"
+            variant="contained"
+            style={{
+              marginRight: '30px',
+              marginTop: '550px',
+              backgroundColor: '#FEAD55',
+              fontSize: '40px',
+              padding: '0 12px',
+              width: '15vw',
+              height: '15vh',
+              borderRadius: '15px',
+            }}
+            onClick={handleHome}
           >
-            <Button className="button-1"></Button>
-            <Button className="button-2"></Button>
-          </Stack>
-        </Complete>
+            홈으로
+          </Button>
+          <Button
+            className="button-2"
+            variant="contained"
+            style={{
+              marginLeft: '30px',
+              marginTop: '550px',
+              backgroundColor: '#FEAD55',
+              fontSize: '40px',
+              padding: '0 12px',
+              width: '15vw',
+              height: '15vh',
+              borderRadius: '15px',
+            }}
+            onClick={handleRe}
+          >
+            다시하기
+          </Button>
+        </Stack>
       </div>
     </StyledComplete>
   );
