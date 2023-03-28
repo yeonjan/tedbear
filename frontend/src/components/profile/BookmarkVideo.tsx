8cd3b909d39674f4a3592e7d0d1579c2cd015ba4
jyuha <jyuha759@gmail.com>
2023-03-28 12:43:17 +0900

    🎨 Navigate to Learning Page

--- frontend/src/components/profile/BookmarkVideo.tsx
+++ frontend/src/components/profile/BookmarkVideo.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getVideoBookmark } from 'utils/api/bookmarkApi';
+import VideoLevel from 'assets/img/videoLevel.svg';
+import BookmarkFull from 'assets/img/bookmarkFull.svg';
+import BookmarkEmpty from 'assets/img/bookmarkEmpty.svg';
 
 interface IBookmarkVideo {
   thumbnailUrl: string;
@@ -18,7 +22,12 @@ const BookIn = styled.div`
 `;
 
 const BookmarkVideo = () => {
+  const navigate = useNavigate();
   const [videoBookmark, setVideoBookmark] = useState<IBookmarkVideo[]>([]);
+
+  const handleClick = (watchId: string): void => {
+    navigate(`/learning/${watchId}`);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,6 +43,33 @@ const BookmarkVideo = () => {
       <div>
         <h2>Bookmarked Video</h2>
         {/* {videoBookmark} */}
+        <div>
+          {videoBookmark.map((Thumnail, idx) => {
+            return (
+              <div className="wrapper" key={idx}>
+                <img
+                  className="main-img"
+                  src={Thumnail.thumbnailUrl}
+                  onClick={() => handleClick(Thumnail.watchId)}
+                  alt=""
+                />
+                <img
+                  src={VideoLevel}
+                  className="video-level"
+                  style={{
+                    filter:
+                      'invert(45%) sepia(78%) saturate(1707%) hue-rotate(161deg) brightness(93%) contrast(103%)',
+                  }}
+                ></img>
+                <img
+                  src={Thumnail.bookMarked ? BookmarkFull : BookmarkEmpty}
+                  className="book-mark"
+                ></img>
+                <div className="title">{Thumnail.title}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </BookIn>
   );
