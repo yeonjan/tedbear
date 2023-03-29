df50a8854c1eb54e7086ff4e5f95b6af9cead0b9
정혜주 <fixup719@gmail.com>
2023-03-29 14:02:02 +0900

    :lipstick: landing ui 수정

--- frontend/src/components/learning/dictionaryModal.tsx
+++ frontend/src/components/learning/dictionaryModal.tsx
@@ -1,28 +1,27 @@
-import styled from 'styled-components';
-import ModalBg from 'assets/img/ModalBg.svg';
-import { ReactComponent as KakaoImg } from 'assets/img/KakaoImg.svg';
-import { ReactComponent as NaverIcon } from 'assets/img/NaverIcon.svg';
-import { ReactComponent as GoogleIcon } from 'assets/img/GoogleIcon.svg';
-import { useNavigate } from 'react-router-dom';
-import LoginBear from 'assets/img/LoginBear.svg';
-import cloud from 'assets/img/cloud.svg';
-import { device } from 'utils/mediaQuery';
-import axios from 'axios';
+import styled, { css } from 'styled-components';
+import Search from 'assets/img/search.svg';
+import { useEffect, useState } from 'react';
+import { getSearchWord, SearchedWord } from 'utils/api/learningApi';
 
 interface Props {
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface MenuProps {
+  menu: number;
+}
+
 const Wrapper = styled.div`
   background-color: ${props => props.theme.mainColor};
   width: 400px;
-  height: 400px;
+  height: 450px;
   z-index: 6;
   position: absolute;
   bottom: 56px;
   right: 56px;
   border-radius: 24px;
   box-shadow: 6px 6px 8px #00000042;
+  transition: 1s;
 `;
 
 const SearchBox = styled.div`
@@ -32,6 +31,7 @@ const SearchBox = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: end;
+  position: relative;
 `;
 
 const ContentBox = styled.div`
@@ -43,12 +43,20 @@ const ContentBox = styled.div`
   justify-content: center;
 `;
 
-const SearchBar = styled.div`
+const SearchBar = styled.input`
   background-color: ${props => props.theme.whiteColor};
   width: 90%;
   height: 60%;
   border-radius: 50px;
   box-shadow: 6px 6px 8px #00000042;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  padding: 8px 16px;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const SearchResult = styled.div`
@@ -57,6 +65,22 @@ const SearchResult = styled.div`
   height: 90%;
   border-radius: 24px;
   box-shadow: 6px 6px 8px #00000042;
+  padding: 8px 20px;
+`;
+
+const SearchImg = styled.img`
+  width: 20px;
+  cursor: pointer;
+  position: absolute;
+  right: calc(100% - 370px);
+  height: calc(100% - 24px);
+`;
+
+const MeanBox = styled.div`
+  width: 100%;
+  height: 85%;
+  padding: 16px;
+  border: 1px solid red;
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
@@ -68,16 +92,139 @@ const SearchResult = styled.div`
     background-color: ${props => props.theme.mainLightColor};
     border-radius: 10px;
   }
+
+  p:nth-child(1) {
+    font-weight: bold;
+    font-size: 20px;
+    padding-bottom: 4px;
+  }
+
+  p:nth-child(2) {
+    font-size: 14px;
+  }
+`;
+
+const SentenceBox = styled.div`
+  border: 1px solid blue;
+  width: 100%;
+  height: 85%;
+  padding: 16px;
+`;
+
+const Menu = styled.div<MenuProps>`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: first baseline;
+  border-bottom: 1px solid ${props => props.theme.blackColorLight2};
+  border: 1px solid red;
+
+  div {
+    width: 80px;
+    margin-right: 16px;
+    border-radius: 50px;
+    text-align: center;
+    padding: 8px;
+    cursor: pointer;
+    font-size: 14px;
+
+    &:hover {
+      background-color: ${props => props.theme.pointLigntGrdColor3};
+    }
+  }
+
+  ${props => {
+    if (props.menu == 2) {
+      return css`
+        div:nth-child(1) {
+          background-color: #ebebeb;
+        }
+        div:nth-child(2) {
+          background-color: ${props => props.theme.pointLigntGrdColor3};
+        }
+      `;
+    } else {
+      return css`
+        div:nth-child(2) {
+          background-color: #ebebeb;
+        }
+        div:nth-child(1) {
+          background-color: ${props => props.theme.pointLigntGrdColor3};
+        }
+      `;
+    }
+  }}
 `;
 
 const DictionaryModal = ({ setOpenModal }: Props) => {
+  const [keyword, setKeyword] = useState<string>('');
+  const [word, setWord] = useState<string>('');
+  const [wordDesc, setWordDesc] = useState<SearchedWord | null>(null);
+
+  const onSetKeyword = (el: any) => {
+    setKeyword(el.target.value);
+  };
+
+  const onSearchWord = () => {
+    const fetchData = async () => {
+      const data = await getSearchWord(keyword, 3, 1);
+      setWordDesc(data);
+    };
+    fetchData();
+
+    setWord(keyword);
+
+    // onRegexMean();
+  };
+
+  // 단어 의미 정리
+  // const [wordMean, setWordMean] = useState<string[]>();
+  // const onRegexMean = async () => {
+  //   console.log('?????');
+  //   const meanArr = wordDesc?.wordInfo.mean.split(/[0-9]/);
+  //   await setWordMean(meanArr);
+  // };
+
+  // 메뉴 버튼
+  const [menu, setMenu] = useState<number>(1);
+
   return (
     <Wrapper>
       <SearchBox>
-        <SearchBar></SearchBar>
+        <SearchBar
+          placeholder="영어 단어를 입력하세요."
+          onChange={el => onSetKeyword(el)}
+        />
+        <SearchImg src={Search} onClick={onSearchWord} />
       </SearchBox>
       <ContentBox>
-        <SearchResult></SearchResult>
+        <SearchResult>
+          <Menu menu={menu}>
+            <div onClick={() => setMenu(1)}>의미</div>
+            <div onClick={() => setMenu(2)}>예문</div>
+          </Menu>
+          {menu == 2 ? (
+            <SentenceBox>예문</SentenceBox>
+          ) : (
+            <MeanBox>
+              {wordDesc && <p>{word}</p>}
+              <p>
+                {wordDesc?.wordInfo.mean}
+                {/* {wordMean?.map((el, index) => {
+                  if (index != 0) {
+                    return (
+                      <>
+                        {index}
+                        {el} <br />
+                      </>
+                    );
+                  }
+                })} */}
+              </p>
+            </MeanBox>
+          )}
+        </SearchResult>
       </ContentBox>
     </Wrapper>
   );
