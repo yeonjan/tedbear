aff48dc9e21b02195307ac05eb9e75234e7def96
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2023-03-30 21:16:00 +0900

    :art: 잠깐 권한 열어놓음

--- backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/SecurityConfig.java
@@ -71,9 +71,10 @@ public class SecurityConfig {
 			.antMatchers(HttpMethod.OPTIONS, "/**/*")
 			.permitAll() // options 메서드 열어두기! Authorization 헤더를 사용하기 때문에 Preflight 넣어줌
 
-			.antMatchers(HttpMethod.GET, "/api/**") // GET요청은 열어두고
-			.permitAll()
-			.anyRequest().authenticated() // 그 외 요청은 권한확인
+				.anyRequest().permitAll()
+//			.antMatchers(HttpMethod.GET) // GET요청은 열어두고
+//			.permitAll()
+//			.anyRequest().authenticated() // 그 외 요청은 권한확인
 			.and()
 			.logout()
 			.logoutSuccessUrl("/")
