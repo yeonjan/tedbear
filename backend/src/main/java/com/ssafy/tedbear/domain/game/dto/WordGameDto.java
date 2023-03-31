777be6d0ef16781c3fdc5cdf76c7a7ef8babbc46
yeonjin <y_jin99@naver.com>
2023-03-31 13:03:49 +0900

    :zap: 응답값에 문장 번역본 추가

--- backend/src/main/java/com/ssafy/tedbear/domain/game/dto/WordGameDto.java
+++ backend/src/main/java/com/ssafy/tedbear/domain/game/dto/WordGameDto.java
@@ -11,6 +11,7 @@ public class WordGameDto {
 	Long wordNo;
 	String answer;
 	String sentence;
+	String translation;
 	int score;
 
 	WordGameDto.Hint hint;
@@ -18,7 +19,8 @@ public class WordGameDto {
 	public WordGameDto(Word word, Sentence sentence) {
 		this.wordNo = word.getNo();
 		this.answer = word.getContent();
-		this.sentence = sentence.getContent().replaceAll("(?i)"+word.getContent(), "tedbear");
+		this.sentence = sentence.getContent().replaceAll("(?i)" + word.getContent(), "tedbear");
+		this.translation = sentence.getTranslation();
 		this.score = sentence.getScore();
 		this.hint = new Hint(sentence);
 	}
