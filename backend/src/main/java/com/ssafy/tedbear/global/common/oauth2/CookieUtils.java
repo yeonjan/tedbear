6c880035a8e60e40d5137cf06cfd7825b56a0d6a
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2023-04-02 21:09:28 +0900

    :loud_sound: 들어온 헤더 확인 로그추가

--- backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/CookieUtils.java
+++ backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/CookieUtils.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 public class CookieUtils {
 
 	public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+		log.info("들어온 request: {}", request);
 		Cookie[] cookies = request.getCookies();
 		if (cookies != null && cookies.length > 0) {
 			for (Cookie cookie : cookies) {
