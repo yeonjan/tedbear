7f541ce1237d63fe3bc0ce8ac3021dfb2a55fcc4
정혜주 <fixup719@gmail.com>
2023-04-03 11:00:31 +0900

    :sparkle: 변경 추적 코드 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,38 +34,31 @@ function App() {
     <>
       <ThemeProvider theme={!toggle ? lightTheme : darkTheme}>
         <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route
-              element={<LayoutPage toggle={toggle} setToggle={setToggle} />}
-            >
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/game" element={<GamePage />} />
-              <Route path="/game/select" element={<GameSelectPage />} />
-              <Route path="/game/detail" element={<GameDetailPage />} />
-              <Route path="/game/complete" element={<GameCompletePage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/learning/:videoId" element={<LearningPage />} />
-              <Route path="/bookmark" element={<BookPage />} />
-              <Route path="/still-learn" element={<StillLearningPage />} />
-              <Route path="/search/:content?" element={<SearchPage />} />
-              <Route path="/cross-word" element={<CrossWordPage />} />
-              <Route
-                path="/cross-word/cover"
-                element={<CrossWordCoverPage />}
-              />
-              <Route path="/level" element={<LevelPage />} />
-            </Route>
-            <Route path="/seung" element={<SeungPage />} />
-            <Route path="/yuha" element={<YuhaPage />} />
-            <Route path="/ju" element={<JuPage />} />
-            <Route
-              path="/"
-              // element={<LandingPage toggle={toggle} setToggle={setToggle} />}
-              element={<LandingPage toggle={toggle} setToggle={setToggle} />}
-            />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route element={<LayoutPage toggle={toggle} setToggle={setToggle} />}>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/game" element={<GamePage />} />
+            <Route path="/game/select" element={<GameSelectPage />} />
+            <Route path="/game/detail" element={<GameDetailPage />} />
+            <Route path="/game/complete" element={<GameCompletePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/learning/:videoId" element={<LearningPage />} />
+            <Route path="/bookmark" element={<BookPage />} />
+            <Route path="/still-learn" element={<StillLearningPage />} />
+            <Route path="/search/:content?" element={<SearchPage />} />
+            <Route path="/cross-word" element={<CrossWordPage />} />
+            <Route path="/cross-word/cover" element={<CrossWordCoverPage />} />
+            <Route path="/level" element={<LevelPage />} />
+          </Route>
+          <Route path="/seung" element={<SeungPage />} />
+          <Route path="/yuha" element={<YuhaPage />} />
+          <Route path="/ju" element={<JuPage />} />
+          <Route
+            path="/"
+            // element={<LandingPage toggle={toggle} setToggle={setToggle} />}
+            element={<LandingPage toggle={toggle} setToggle={setToggle} />}
+          />
+        </Routes>
       </ThemeProvider>
     </>
   );
