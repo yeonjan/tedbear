8f89f5fc3a0b7548798236eb9491dde77e069610
박서영 <gksdidtk123@naver.com>
2023-04-03 13:56:18 +0900

    🐛import ResponseCookie 코드추가

--- backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/OAuth2LoginSuccessHandler.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
