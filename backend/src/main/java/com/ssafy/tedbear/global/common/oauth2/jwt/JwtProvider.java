a9df3991beb24b133a918c1690883c8abeb2608b
박서영 <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2023-04-04 00:06:45 +0900

    :white_check_mark: access 10초 TEST

--- backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/jwt/JwtProvider.java
+++ backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/jwt/JwtProvider.java
@@ -30,7 +30,7 @@ public class JwtProvider {
 
 	//  public static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 30; // 30분
 	// public static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 10; // 10초
-	public static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 60; // 10초
+	public static final Long ACCESS_TOKEN_VALIDATE_TIME = 1000L * 10; // 10초
 	public static final Long REFRESH_TOKEN_VALIDATE_TIME = 1000L * 60 * 60 * 24 * 7; // 7일
 	private final String AUTHORITIES_KEY = "role";
 
