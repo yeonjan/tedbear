a4a7c44fe7a704930b4e0fd6bc5fb649f2bca8a9
seunghwan <asdryzx@naver.com>
2023-04-03 17:54:40 +0900

    :art: badge 변경

--- frontend/src/components/short/ShortsPageNation.tsx
+++ frontend/src/components/short/ShortsPageNation.tsx
@@ -1,45 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Shorts } from 'utils/api/recommApi';
-import VideoLevel from 'assets/img/videoLevel.svg';
 import buttonLeft from 'assets/img/buttonLeft.svg';
 import buttonRight from 'assets/img/buttonRight.svg';
+import Badge from 'components/common/Badge';
 
-interface BadgeProps {
-  score?: number;
-}
-
-const ViedoLevelImg = styled.img<BadgeProps>`
+const ViedoLevelImg = styled.div`
   height: 15%;
   width: 15%;
   position: absolute;
   top: 4%;
   left: 4%;
-  filter: ${props => {
-    if (props.score == 0) {
-      return `${props.theme.badgeRed}`;
-    } else if (props.score == 1) {
-      return `${props.theme.badgeOrange}`;
-    } else if (props.score == 2) {
-      return `${props.theme.badgeYellow}`;
-    } else if (props.score == 3) {
-      return `${props.theme.badgeGreen}`;
-    } else if (props.score == 4) {
-      return `${props.theme.badgeBlue}`;
-    } else if (props.score == 5) {
-      return `${props.theme.badgeIndigo}`;
-    } else if (props.score == 6) {
-      return `${props.theme.badgePurple}`;
-    } else if (props.score == 7) {
-      return `${props.theme.badgeBronze}`;
-    } else if (props.score == 8) {
-      return `${props.theme.badgeSilver}`;
-    } else if (props.score == 9) {
-      return `${props.theme.badgGold}`;
-    } else {
-      return `${props.theme.badgeUnlank}`;
-    }
-  }};
 `;
 
 const Containter = styled.div`
@@ -128,7 +99,9 @@ const ShortsPageNation = ({
                 setShortsId(Thumnail);
               }}
             ></img>
-            <ViedoLevelImg src={VideoLevel} score={Thumnail.score} />
+            <ViedoLevelImg>
+              <Badge score={Thumnail.score} />
+            </ViedoLevelImg>
           </div>
         );
       })}
