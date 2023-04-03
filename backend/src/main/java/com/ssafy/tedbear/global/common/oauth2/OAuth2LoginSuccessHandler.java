902c62a87e4db5f07e62ccfc32b7b2c206806fc2
박서영 <gksdidtk123@naver.com>
2023-04-03 12:26:49 +0900

    ✅ redirect_url j8b -> localhost

--- backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/ssafy/tedbear/global/common/oauth2/OAuth2LoginSuccessHandler.java
@@ -44,7 +44,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 		clearAuthenticationAttributes(request, response);
 
 		getRedirectStrategy().sendRedirect(request, response,
-			"https://j8b103.p.ssafy.io/seung?accessToken=" + accessToken + "&refreshToken=" + refreshToken
+			"http://localhost:3000/seung?accessToken=" + accessToken + "&refreshToken=" + refreshToken
 				+ "&join="
 				+ join); // 난이도 측정 페이지로 이동(프론트에서 분기)
 
