e281842762b1b4d614afdccd419da43a85a11ea3
repeater1384 <repeater1384@naver.com>
2023-04-04 14:42:43 +0900

    a

--- python_project/word_puzzle_generator.py
+++ python_project/word_puzzle_generator.py
@@ -6,7 +6,7 @@ conn = pymysql.connect(host='3.36.50.141', user='root', password='824b2937-79c0-
                        database='tedbearDB',
                        autocommit=True)
 cursor = conn.cursor()
-sql = 'SELECT * FROM tedbearDB.word_tb where length(content) between 4 and 7 and score != 0 order by rand() limit 100'
+sql = 'SELECT * FROM tedbearDB.word_tb where length(content) between 3 and 8 and score != 0 order by rand() limit 1000'
 cursor.execute(sql)
 word_list = [*cursor.fetchall()]
 
@@ -20,30 +20,29 @@ for word in word_list:
     else:
         print(word)
 
-use_word_list = use_word_list[:10]
-
-board = [['.'] * 100 for _ in range(100)]
+use_word_list.sort(key=lambda x: -len(x[0]))
+size = 50
+board = [['.'] * 50 for _ in range(50)]
 
 sero_candidate = {}
 next_char = None
-y_pos = 50
-x_pos = 45
+y_pos = size // 2
+x_pos = size // 2 - 3
 for _ in range(1):
-    choice_word, choice_word_mean = use_word_list.pop(random.randint(0, len(use_word_list) - 1))
+    # choice_word, choice_word_mean = use_word_list.pop(random.randint(0, len(use_word_list) - 1))
+    choice_word, choice_word_mean = use_word_list.pop(0)
     for j in range(len(choice_word)):
         board[y_pos][x_pos + j] = choice_word[j]
 
     j = 0
+
     while j < len(choice_word):
-        next_word, next_mean = use_word_list.pop()
+        next_word, next_mean = use_word_list.pop(0)
         if choice_word[j] in next_word:
             temp = next_word.find(choice_word[j])
-            for a in range(len(next_word)):
-                board[y_pos + a - temp][x_pos + j] = next_word[a]
-            j += 2
-        else:
-            j += 1
+            is_sharp = random.choice([True, False])
+            if (is_sharp and temp <= 2) or (not is_sharp and temp >= 6):
+                for a in range(len(next_word)):
+                    board[y_pos + a - temp][x_pos + j] = next_word[a]
+                j += random.choice([2, 3, 4])
 print(*map(''.join, board), sep='\n')
-
-for use_word, mean in use_word_list:
-    print(use_word + ', ')
