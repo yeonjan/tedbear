e7c2bb94ac45c65d52aae4fd42422c240811eacc
repeater1384 <repeater1384@naver.com>
2023-04-11 09:17:32 +0900

    :necktie: 최종발표 대비 수정

--- backend/src/main/java/com/ssafy/tedbear/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/ssafy/tedbear/domain/member/service/MemberServiceImpl.java
@@ -3,7 +3,6 @@ package com.ssafy.tedbear.domain.member.service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -89,30 +88,34 @@ public class MemberServiceImpl implements MemberService {
 		// 2만번대 난이도의 문장, 3만번대 난이도의 문장 , ... , 7만번대 난이도의 문장 각 1개씩 총 6개 반환하기
 		long randomNo = Math.abs(random.nextLong()) % 300000;
 
-		// 스트림 사용 안하고 구현한 버전
-		List<Sentence> initSentenceList = sentenceRepository.findByNoBetweenAndScoreBetween(randomNo, randomNo + 10000,
-			20000, 79999);
-		Map<Integer, List<Sentence>> scoreGroupSentence = new HashMap<>();
-		for (Sentence sentence : initSentenceList) {
-			int score = sentence.getScore();
-			int scoreGroup = score / 10000;
-			if (!scoreGroupSentence.containsKey(scoreGroup))
-				scoreGroupSentence.put(scoreGroup, new ArrayList<>());
-			scoreGroupSentence.get(scoreGroup).add(sentence);
-		}
-
-		List<Sentence> selectedSentenceList = new ArrayList<>();
-		for (int key : scoreGroupSentence.keySet()) {
-			List<Sentence> keySentenceList = scoreGroupSentence.get(key);
-			int randomIdx = random.nextInt(keySentenceList.size());
-			selectedSentenceList.add(keySentenceList.get(randomIdx));
-		}
+		// // 스트림 사용 안하고 구현한 버전
+		// List<Sentence> initSentenceList = sentenceRepository.findByNoBetweenAndScoreBetween(randomNo, randomNo + 10000,
+		// 	20000, 79999);
+		// Map<Integer, List<Sentence>> scoreGroupSentence = new HashMap<>();
+		// for (Sentence sentence : initSentenceList) {
+		// 	int score = sentence.getScore();
+		// 	int scoreGroup = score / 10000;
+		// 	if (!scoreGroupSentence.containsKey(scoreGroup))
+		// 		scoreGroupSentence.put(scoreGroup, new ArrayList<>());
+		// 	scoreGroupSentence.get(scoreGroup).add(sentence);
+		// }
+		//
+		// List<Sentence> selectedSentenceList = new ArrayList<>();
+		// for (int key : scoreGroupSentence.keySet()) {
+		// 	List<Sentence> keySentenceList = scoreGroupSentence.get(key);
+		// 	int randomIdx = random.nextInt(keySentenceList.size());
+		// 	selectedSentenceList.add(keySentenceList.get(randomIdx));
+		// }
 
 		// 스트림 사용한 버전
 		List<Sentence> problemSentenceList =
 			sentenceRepository.findByNoBetweenAndScoreBetween(randomNo, randomNo + 10000, 20000, 79999)
 				.stream().collect(Collectors.groupingBy(x -> x.getScore() / 10000)).values().stream()
-				.map(x -> x.stream().skip(random.nextInt(x.size() - 1)).limit(1).collect(Collectors.toList()))
+				.map(x -> x.stream()
+					.filter(y -> y.getContent().length() >= 40 && y.getContent().length() <= 50)
+					// .skip(random.nextInt(x.size() - 1))
+					.limit(1)
+					.collect(Collectors.toList()))
 				.flatMap(innerStream -> innerStream.stream()).collect(Collectors.toList());
 
 		return new ProblemListDto(problemSentenceList, problemWordList);
--- backend/src/main/java/com/ssafy/tedbear/domain/video/repository/VideoRepository.java
+++ backend/src/main/java/com/ssafy/tedbear/domain/video/repository/VideoRepository.java
@@ -14,9 +14,11 @@ import com.ssafy.tedbear.domain.video.entity.Video;
 public interface VideoRepository extends JpaRepository<Video, Long> {
 	Video findByWatchId(String watchId);
 
-	@Query("select v from Video v where v.score between :startScore1 and :endScore1 or v.score between :startScore2 and :endScore2 or v.score between :startScore3 and :endScore3")
-	List<Video> findByScoreBetween(int startScore1, int endScore1, int startScore2, int endScore2, int startScore3,
-		int endScore3);
+	@Query(value = "select v from Video v where " +
+		"v.score between :s1 and :e1 or " +
+		"v.score between :s2 and :e2 or " +
+		"v.score between :s3 and :e3")
+	List<Video> findByScoreBetween(int s1, int e1, int s2, int e2, int s3, int e3);
 
 	@Query("select v from Video v where v.title like %:query%")
 	Slice<Video> findSliceByTitle(String query, Pageable pageable);
